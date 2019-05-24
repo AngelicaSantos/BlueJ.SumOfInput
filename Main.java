06bd755f5e241a6ddb62f953175cb74d97fe08dc
Angelica Santos <angelicasantos@zipcoders-MacBook-Pro-4.local>
2019-05-24 14:41:02 -0400

    complete

--- Main.java
+++ Main.java
@@ -1,9 +1,33 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
-public class Main {
+import java.util.Scanner; 
+public class Main 
+{
+public static void main(String[] args) {
+   
+   int num= 1;
+   int sum= 0;
+   Scanner in = new Scanner(System.in);
+   num = in.nextInt();
+   System.out.println("Please enter a number");
+   for (int i =1; i <= num; i++)
+   {
+       
+       sum += i;
+       
+       
 
-    public static void main(String[] args){
-
+       
     }
+    System.out.println ( "sum =" + sum );
+  
+ 
+  
+    
+    
+    
 }
+
+}
+        
